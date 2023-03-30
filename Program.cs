15fec88aee11ef22ce8e71451f3a85c3225ac82f
Jackie <jackie@scantist.com>
2023-03-30 17:28:22 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json;
+public class Person
+{
+    public string? Name { get; set; }
+    public int Age { get; set; }
+}
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        var person = new Person { Name = "John", Age = 30 };
+        string jsonString = JsonConvert.SerializeObject(person);
+        Console.WriteLine(jsonString);
+    }
+}
